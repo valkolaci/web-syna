f983d3278e40fc4cfa005e8bd357bbf4292d380e
valkolaci <valkolaci@users.noreply.github.com>
2020-07-22 19:46:29 +0000

    deploy: 6681e7278fd39d86367d386144e949f8f9a909e9

--- scripts/front.min.4a3232bb60c123b2d7d1b8abeae1aed142dace7461d6aa4a000b87bba9ce25e0.js
+++
@@ -1,7 +0,0 @@
-$(function(){sliderHomepage()
-sliders()})
-function sliderHomepage(){if($('#slider').length){$('#slider').owlCarousel({autoPlay:3000,items:4,itemsDesktopSmall:[900,3],itemsTablet:[600,3],itemsMobile:[500,2]})}}
-function sliders(){if($('.owl-carousel').length){$('.customers').owlCarousel({items:6,itemsDesktopSmall:[990,4],itemsTablet:[768,2],itemsMobile:[480,1]})
-$('.testimonials').owlCarousel({items:4,itemsDesktopSmall:[990,3],itemsTablet:[768,2],itemsMobile:[480,1]})
-$('.project').owlCarousel({navigation:true,navigationText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],slideSpeed:300,paginationSpeed:400,autoPlay:true,stopOnHover:true,singleItem:true,afterInit:'',lazyLoad:true})
-$('.homepage').owlCarousel({navigation:false,navigationText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],slideSpeed:2000,paginationSpeed:1000,autoPlay:true,stopOnHover:true,singleItem:true,lazyLoad:false,addClassActive:true,afterInit:function(){},afterMove:function(){}})}}
